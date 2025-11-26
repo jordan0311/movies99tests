5e4453d47cc22b9820b18a9c8dbafdd2d7c7ccb4
jordan0311 <220813255+jordan0311@users.noreply.github.com>
2025-11-25 21:19:00 -0500

    movies99testsActual1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,5 @@
 import { Router } from 'express';
 import apiRoutes from './api/index.js';
-import directoresRouter from './api/directores.routes.js';
-import actorRouter from './api/actor.routes.js';
-import genreRouter from './api/generos.routes.js';
-import movieRouter from './api/movies.routes.js';
 
 const router = Router();
 
@@ -12,9 +8,5 @@ router.get('/', (req, res) => {
 });
 
 router.use('/api', apiRoutes);
-router.use('/api/movies', movieRouter);
-router.use('/api/genres', genreRouter);
-router.use('/api/actors', actorRouter);
-router.use('/api/directors', directoresRouter);
 
 export default router;
